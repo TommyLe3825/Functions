3746a794aa233157bc7c8c828941bc0144c529e6
Tommy Le <tommyle1624@gmail.com>
2023-12-20 08:28:13 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//your code goes here
+template <typename T>T myMax(T x, T y){
+    return (x > y) ? x : y; //if true, it's x and if not, it's y
+}
+
+int main () {
+    double x, y;
+    cin>>x>>y;
+    int a, b;
+    cin>>a>>b;
+    cout << myMax(x, y) << endl;
+    cout << myMax(a, b) << endl;
+}
